67f3b7dba61d850e814777bb08f2623105669c44
Akshat-WorkHub <akshatsoftware9829@gmail.com>
2026-05-17 11:44:14 +0530

    First Commit on 17 May 2026

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPHEN = "-e ."
+
+def get_requirements(filepath: str) -> List[str]:
+    '''
+        Input: requirements.txt filepath
+        Output: List of required libraries to install
+    '''
+    req = []
+    with open(filepath,'r') as file:
+        req = file.readlines()
+        req = [r.replace("\n","").strip() for r in req]
+        
+        if HYPHEN in req:
+            req.remove(HYPHEN)
+
+        print(req)
+        
+
+
+
+# setup(
+#     name='IntershipPredictor', # Project Name
+#     version='0.0.1', # Project Version
+#     author="Akshat Agarwal", 
+#     author_email="akshatsoftware9829@gmail.com",
+#     packages=find_packages(), # Identifies what folders can act as packages having __init__ file in it 
+#     install_requires=[] # Exctract All files that need to be downloaded from requirements.txt filepath
+# )
+
+if __name__ == "__main__":
+    get_requirements('requirements.txt')
